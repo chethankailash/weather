202105ada5fab87a270f51f1048145ac93586b97
chethan kailash <chethankailash3@gmail.com>
2021-12-26 18:52:47 -0600

    Refactor

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 #Temparature is a python project where it will display current/given city weather data.
 # using OPEN Weather API
 # using location API 
+
+USAGE:
+
+i) python3 temperature.py  
+ii) python3 temperature.py <city name> (ex: python3 temperature.py Bangalore)
--- url.py
+++ url.py
@@ -5,18 +5,11 @@ Url = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=a61bc3a5290
 
 def URL(city):
     City= getCity(city)
-    # print("===")
-    # print(City)
     Url = "http://api.openweathermap.org/data/2.5/weather?q={c}&appid={}".format(apikey.apiKey,c=City)
     return Url
     
 def getCity(city):
     if city == "current":
-        # Latitude = '41.8781'
-        # Longitude = '87.6298'
-        # geolocator = Nominatim(user_agent="geoapiForTemperatures")
-        # location = geolocator.reverse("41.8781,87.6298")
-        # return location
         return "Chicago"
     else:
         return city
